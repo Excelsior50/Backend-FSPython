18d00e846fbb7e032d2c523195f8661efad64a2e
Excelsior <ericchristensen59@gmail.com>
2024-07-01 14:41:29 -0300

    Rama finalizada db proyecto, con tiendita_db en sql

--- sql/tablesWhitCreateSchema.sql
+++ sql/tablesWhitCreateSchema.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `tiendita_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `tiendita_db`;
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: tiendita_db
@@ -142,4 +144,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-07-01 10:16:14
+-- Dump completed on 2024-07-01 10:28:32
